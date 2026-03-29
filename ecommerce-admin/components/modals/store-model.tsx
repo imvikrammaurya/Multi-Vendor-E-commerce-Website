aba56ceb2cebc53d83309e5d11f9cee492e87e6f
imvikrammaurya <imvikrammaurya@outlook.com>
2026-03-29 23:35:20 +0530

    fixing things here and there in my current project and figuring out some errors

--- ecommerce-admin/components/modals/store-model.tsx
+++ ecommerce-admin/components/modals/store-model.tsx
@@ -4,11 +4,16 @@ import { useStoreModal } from "@/hooks/use-store-model";
 import { Modal } from "@/components/ui/model";
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
+  const storeModal = useStoreModal();
 
-    return (
-  <Modal title="Create SStore" description="Add a new store to manage products and categories" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-    Future Create Store Form
-  </Modal>
-    );
-}
+  return (
+    <Modal
+      title="Create SStore"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Future Create Store Form
+    </Modal>
+  );
+};
